dff2c7ee72b95cd130652e032a94c9d2694b03ff
myster <abbeymyster@gmail.com>
2024-12-05 12:27:56 +0000

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  return (
+    <div>
+      <h1>Data List</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
